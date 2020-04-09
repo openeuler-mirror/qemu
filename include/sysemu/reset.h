aa311930c2539f6c46da41905ed4cc4432c351f2
Keqian Zhu <zhukeqian1@huawei.com>
2020-04-09 09:31:22 +0800

    hw/arm/boot: Add manually register and trigger of CPU reset
    We need to register and trigger CPU reset manually for hotplugged
    CPU. Besides, we gather CPU reset handlers of all CPUs because CPU
    reset should happen before GIC reset.
    
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>

--- include/sysemu/reset.h
+++ include/sysemu/reset.h
@@ -2,7 +2,11 @@
 #define QEMU_SYSEMU_RESET_H
 
 typedef void QEMUResetHandler(void *opaque);
+typedef struct QEMUResetEntry QEMUResetEntry;
 
+QEMUResetEntry *qemu_get_reset_entry(QEMUResetHandler *func, void *opaque);
+void qemu_register_reset_after(QEMUResetEntry *entry,
+                               QEMUResetHandler *func, void *opaque);
 void qemu_register_reset(QEMUResetHandler *func, void *opaque);
 void qemu_unregister_reset(QEMUResetHandler *func, void *opaque);
 void qemu_devices_reset(void);
