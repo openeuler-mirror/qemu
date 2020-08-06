e55671b1d033f8815316407e0274fd85f48bc4df
Peng Liang <liangpeng10@huawei.com>
2020-08-06 16:14:55 +0800

    target/arm: introduce KVM_CAP_ARM_CPU_FEATURE
    Introduce KVM_CAP_ARM_CPU_FEATURE to check whether KVM supports to set
    CPU features in ARM.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Peng Liang <liangpeng10@huawei.com>

--- target/arm/kvm64.c
+++ target/arm/kvm64.c
@@ -644,6 +644,20 @@ bool kvm_arm_get_host_cpu_features(ARMHostCPUFeatures *ahcf)
     return true;
 }
 
+bool kvm_arm_cpu_feature_supported(void)
+{
+    static bool cpu_feature_initialized;
+    static bool cpu_feature_supported;
+
+    if (!cpu_feature_initialized) {
+        cpu_feature_supported = kvm_check_extension(kvm_state,
+                                                    KVM_CAP_ARM_CPU_FEATURE);
+        cpu_feature_initialized = true;
+    }
+
+    return cpu_feature_supported;
+}
+
 #define ARM_CPU_ID_MPIDR       3, 0, 0, 0, 5
 
 int kvm_arch_init_vcpu(CPUState *cs)
