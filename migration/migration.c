5e4bc7ceaf81a4932c92e479e9add947b698395b
Chuan Zheng <zhengchuan@huawei.com>
2021-01-30 15:57:31 +0800

    migration: Add multi-thread compress ops
    Add the MigrationCompressOps and MigrationDecompressOps structures to make
    the compression method configurable for multi-thread compression migration.
    
    Signed-off-by: Chuan Zheng <zhengchuan@huawei.com>
    Signed-off-by: Zeyu Jin <jinzeyu@huawei.com>
    Signed-off-by: Ying Fang <fangying1@huawei.com>

--- migration/migration.c
+++ migration/migration.c
@@ -2456,6 +2456,15 @@ int migrate_decompress_threads(void)
     return s->parameters.decompress_threads;
 }
 
+CompressMethod migrate_compress_method(void)
+{
+    MigrationState *s;
+
+    s = migrate_get_current();
+
+    return s->parameters.compress_method;
+}
+
 bool migrate_dirty_bitmaps(void)
 {
     MigrationState *s;
