48221a8586fc3965dc9efc131ccc45c6369f8a1e
Prasad J Pandit <pjp@fedoraproject.org>
2021-03-25 17:27:07 +0800

    tz-ppc: add dummy read/write methods
    fix CVE-2020-15469
    
    Add tz-ppc-dummy mmio read/write methods to avoid assert failure
    during initialisation.
    
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    
    Signed-off-by: Jiajie Li <lijiajie11@huawei.com>

--- hw/misc/tz-ppc.c
+++ hw/misc/tz-ppc.c
@@ -193,7 +193,20 @@ static bool tz_ppc_dummy_accepts(void *opaque, hwaddr addr,
     g_assert_not_reached();
 }
 
+static uint64_t tz_ppc_dummy_read(void *opaque, hwaddr addr, unsigned size)
+{
+    g_assert_not_reached();
+}
+
+static void tz_ppc_dummy_write(void *opaque, hwaddr addr,
+                                        uint64_t data, unsigned size)
+{
+    g_assert_not_reached();
+}
+
 static const MemoryRegionOps tz_ppc_dummy_ops = {
+    .read = tz_ppc_dummy_read,
+    .write = tz_ppc_dummy_write,
     .valid.accepts = tz_ppc_dummy_accepts,
 };
 
