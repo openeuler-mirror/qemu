ac1bf3edcd2b807cf81ada500716f13b1394d58e
Zenghui Yu <yuzenghui@huawei.com>
2021-05-08 17:31:04 +0800

    vfio: Maintain DMA mapping range for the container
    When synchronizing dirty bitmap from kernel VFIO we do it in a
    per-iova-range fashion and we allocate the userspace bitmap for each of the
    ioctl. This patch introduces `struct VFIODMARange` to describe a range of
    the given DMA mapping with respect to a VFIO_IOMMU_MAP_DMA operation, and
    make the bitmap cache of this range be persistent so that we don't need to
    g_try_malloc0() every time. Note that the new structure is almost a copy of
    `struct vfio_iommu_type1_dma_map` but only internally used by QEMU.
    
    More importantly, the cached per-iova-range dirty bitmap will be further
    used when we want to add support for the CLEAR_BITMAP and this cached
    bitmap will be used to guarantee we don't clear any unknown dirty bits
    otherwise that can be a severe data loss issue for migration code.
    
    It's pretty intuitive to maintain a bitmap per container since we perform
    log_sync at this granule. But I don't know how to deal with things like
    memory hot-{un}plug, sparse DMA mappings, etc. Suggestions welcome.
    
    * yet something to-do:
      - can't work with guest viommu
      - no locks
      - etc
    
    [ The idea and even the commit message are largely inherited from kvm side.
      See commit 9f4bf4baa8b820c7930e23c9566c9493db7e1d25. ]
    
    Signed-off-by: Zenghui Yu <yuzenghui@huawei.com>
    Signed-off-by: Kunkun Jiang <jinagkunkun@huawei.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -76,6 +76,14 @@ typedef struct VFIOAddressSpace {
 
 struct VFIOGroup;
 
+typedef struct VFIODMARange {
+    QLIST_ENTRY(VFIODMARange) next;
+    hwaddr iova;
+    size_t size;
+    void *vaddr; /* unused */
+    unsigned long *bitmap; /* dirty bitmap cache for this range */
+} VFIODMARange;
+
 typedef struct VFIOContainer {
     VFIOAddressSpace *space;
     int fd; /* /dev/vfio/vfio, empowered by the attached groups */
@@ -93,6 +101,7 @@ typedef struct VFIOContainer {
     QLIST_HEAD(, VFIOHostDMAWindow) hostwin_list;
     QLIST_HEAD(, VFIOGroup) group_list;
     QLIST_HEAD(, VFIORamDiscardListener) vrdl_list;
+    QLIST_HEAD(, VFIODMARange) dma_list;
     QLIST_ENTRY(VFIOContainer) next;
 } VFIOContainer;
 
