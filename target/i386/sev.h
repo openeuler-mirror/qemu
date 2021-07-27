f6753191237118294d04193908db503bb87619f7
Brijesh Singh <brijesh.singh@amd.com>
2021-07-27 12:10:23 +0000

    target/i386: sev: provide callback to setup outgoing context
    cherry-picked from https://github.com/AMDESE/qemu/commit/7521883afc0.
    
    The user provides the target machine's Platform Diffie-Hellman key (PDH)
    and certificate chain before starting the SEV guest migration. Cache the
    certificate chain as we need them while creating the outgoing context.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Co-developed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    [ Fix conflict. ]
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- target/i386/sev.h
+++ target/i386/sev.h
@@ -51,6 +51,8 @@ uint32_t sev_get_reduced_phys_bits(void);
 bool sev_add_kernel_loader_hashes(SevKernelLoaderContext *ctx, Error **errp);
 
 int sev_encrypt_flash(uint8_t *ptr, uint64_t len, Error **errp);
+int sev_save_setup(const char *pdh, const char *plat_cert,
+                   const char *amd_cert);
 int sev_inject_launch_secret(const char *hdr, const char *secret,
                              uint64_t gpa, Error **errp);
 
