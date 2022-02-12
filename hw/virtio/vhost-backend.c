1545a60a8b78490c7dc8909b7012bca63dba63cd
Jinhua Cao <caojinhua1@huawei.com>
2022-02-12 15:41:08 +0800

    qmp: add command to query used memslots of vhost-net and vhost-user
    Signed-off-by: Jinhua Cao <caojinhua1@huawei.com>

--- hw/virtio/vhost-backend.c
+++ hw/virtio/vhost-backend.c
@@ -300,7 +300,7 @@ static void vhost_kernel_set_used_memslots(struct vhost_dev *dev)
     vhost_kernel_used_memslots = dev->mem->nregions;
 }
 
-static unsigned int vhost_kernel_get_used_memslots(void)
+unsigned int vhost_kernel_get_used_memslots(void)
 {
     return vhost_kernel_used_memslots;
 }
--- monitor/qmp-cmds.c
+++ monitor/qmp-cmds.c
@@ -36,6 +36,7 @@
 #include "qapi/qapi-commands-machine.h"
 #include "qapi/qapi-commands-misc.h"
 #include "qapi/qapi-commands-ui.h"
+#include "qapi/qapi-commands-net.h"
 #include "qapi/type-helpers.h"
 #include "qapi/qmp/qerror.h"
 #include "exec/ramlist.h"
@@ -43,6 +44,7 @@
 #include "hw/acpi/acpi_dev_interface.h"
 #include "hw/intc/intc.h"
 #include "hw/rdma/rdma.h"
+#include "hw/virtio/vhost-backend.h"
 
 NameInfo *qmp_query_name(Error **errp)
 {
@@ -470,4 +472,14 @@ HumanReadableText *qmp_x_query_irq(Error **errp)
 int64_t qmp_query_rtc_date_diff(Error **errp)
 {
     return get_rtc_date_diff();
+}
+
+uint32_t qmp_query_vhost_kernel_used_memslots(Error **errp)
+{
+    return vhost_kernel_get_used_memslots();
+}
+
+uint32_t qmp_query_vhost_user_used_memslots(Error **errp)
+{
+    return vhost_user_get_used_memslots();
 }
