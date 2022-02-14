1f1c2f74668cd1250cbd00b397dd59be92121314
Eugenio Pérez <eperezma@redhat.com>
2022-02-14 20:34:15 +0100

    vdpa: Make ncs autofree
    Simplifying memory management.
    
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20220214193415.1606752-2-eperezma@redhat.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    Signed-off-by: fangyi <eric.fangyi@huawei.com>

--- net/vhost-vdpa.c
+++ net/vhost-vdpa.c
@@ -271,7 +271,8 @@ int net_init_vhost_vdpa(const Netdev *netdev, const char *name,
 {
     const NetdevVhostVDPAOptions *opts;
     int vdpa_device_fd;
-    NetClientState **ncs, *nc;
+    g_autofree NetClientState **ncs = NULL;
+    NetClientState *nc;
     int queue_pairs, i, has_cvq = 0;
 
     assert(netdev->type == NET_CLIENT_DRIVER_VHOST_VDPA);
@@ -309,7 +310,6 @@ int net_init_vhost_vdpa(const Netdev *netdev, const char *name,
             goto err;
     }
 
-    g_free(ncs);
     return 0;
 
 err:
@@ -317,7 +317,6 @@ err:
         qemu_del_net_client(ncs[0]);
     }
     qemu_close(vdpa_device_fd);
-    g_free(ncs);
 
     return -1;
 }
