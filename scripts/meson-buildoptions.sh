4d572573175449f48fc12c9f9524fc09f219cdbd
ling xu <ling1.xu@intel.com>
2022-11-16 23:29:22 +0800

    AVX512 support for xbzrle_encode_buffer
    mainline inclusion
    from mainline-v8.0.0-rc0
    commit 04ffce137b6d85ab4e7687e54e4dffcef0a9ab99
    category: feature
    feature: AVX512 support for xbzrle_encode_buffer
    bugzilla: https://gitee.com/openeuler/intel-qemu/issues/I6Z50P
    
    Intel-SIG: commit 04ffce137b6d ("AVX512 support for xbzrle_encode_buffer")
    
    -------------------------------------
    
    AVX512 support for xbzrle_encode_buffer
    
    This commit is the same with [PATCH v6 1/2], and provides avx512 support for xbzrle_encode_buffer
    function to accelerate xbzrle encoding speed. Runtime check of avx512
    support and benchmark for this feature are added. Compared with C
    version of xbzrle_encode_buffer function, avx512 version can achieve
    50%-70% performance improvement on benchmarking. In addition, if dirty
    data is randomly located in 4K page, the avx512 version can achieve
    almost 140% performance gain.
    
    Signed-off-by: ling xu <ling1.xu@intel.com>
    Co-authored-by: Zhou Zhao <zhou.zhao@intel.com>
    Co-authored-by: Jun Jin <jun.i.jin@intel.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Aichun Shi <aichun.shi@intel.com>

--- scripts/meson-buildoptions.sh
+++ scripts/meson-buildoptions.sh
@@ -26,6 +26,7 @@ meson_options_help() {
   printf "%s\n" '  attr            attr/xattr support'
   printf "%s\n" '  auth-pam        PAM access control'
   printf "%s\n" '  avx2            AVX2 optimizations'
+  printf "%s\n" '  avx512bw        AVX512BW optimizations'
   printf "%s\n" '  avx512f         AVX512F optimizations'
   printf "%s\n" '  bpf             eBPF support'
   printf "%s\n" '  brlapi          brlapi character device driver'
@@ -111,6 +112,8 @@ _meson_option_parse() {
     --disable-auth-pam) printf "%s" -Dauth_pam=disabled ;;
     --enable-avx2) printf "%s" -Davx2=enabled ;;
     --disable-avx2) printf "%s" -Davx2=disabled ;;
+    --enable-avx512bw) printf "%s" -Davx512bw=enabled ;;
+    --disable-avx512bw) printf "%s" -Davx512bw=disabled ;;
     --enable-avx512f) printf "%s" -Davx512f=enabled ;;
     --disable-avx512f) printf "%s" -Davx512f=disabled ;;
     --enable-bpf) printf "%s" -Dbpf=enabled ;;
