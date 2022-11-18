f32fc48313dadeb6469c00660bd96331e120030f
Kunkun Jiang <jiangkunkun@huawei.com>
2022-11-18 15:22:40 +0800

    Revert "vfio/pci: Register handler for iommu fault"
    This reverts commit 574455d1363e818905e05cd23ef0948e83a16a51.
    
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>

--- hw/vfio/pci.h
+++ hw/vfio/pci.h
@@ -114,12 +114,6 @@ typedef struct VFIOMSIXInfo {
     unsigned long *pending;
 } VFIOMSIXInfo;
 
-typedef struct VFIOPCIExtIRQ {
-    struct VFIOPCIDevice *vdev;
-    EventNotifier notifier;
-    uint32_t index;
-} VFIOPCIExtIRQ;
-
 #define TYPE_VFIO_PCI "vfio-pci"
 OBJECT_DECLARE_SIMPLE_TYPE(VFIOPCIDevice, VFIO_PCI)
 
@@ -144,7 +138,6 @@ struct VFIOPCIDevice {
     PCIHostDeviceAddress host;
     EventNotifier err_notifier;
     EventNotifier req_notifier;
-    VFIOPCIExtIRQ *ext_irqs;
     int (*resetfn)(struct VFIOPCIDevice *);
     uint32_t vendor_id;
     uint32_t device_id;
