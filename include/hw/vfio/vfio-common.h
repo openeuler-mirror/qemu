17190414cd411d23f1fc14c3d44d7b9f210f12b0
Kunkun Jiang <jiangkunkun@huawei.com>
2022-11-18 15:22:42 +0800

    Revert "vfio: Pass stage 1 MSI bindings to the host"
    This reverts commit 8b4fbe869f8a1f510896c86067d2e4fc3dc82eb9.
    
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -74,14 +74,6 @@ typedef struct VFIOAddressSpace {
     QLIST_ENTRY(VFIOAddressSpace) list;
 } VFIOAddressSpace;
 
-typedef struct VFIOMSIBinding {
-    int index;
-    hwaddr iova;
-    hwaddr gpa;
-    hwaddr size;
-    QLIST_ENTRY(VFIOMSIBinding) next;
-} VFIOMSIBinding;
-
 struct VFIOGroup;
 
 typedef struct VFIODMARange {
@@ -111,7 +103,6 @@ typedef struct VFIOContainer {
     QLIST_HEAD(, VFIOGroup) group_list;
     QLIST_HEAD(, VFIORamDiscardListener) vrdl_list;
     QLIST_HEAD(, VFIODMARange) dma_list;
-    QLIST_HEAD(, VFIOMSIBinding) msibinding_list;
     QLIST_ENTRY(VFIOContainer) next;
 } VFIOContainer;
 
@@ -231,9 +222,6 @@ VFIOGroup *vfio_get_group(int groupid, AddressSpace *as, Error **errp);
 void vfio_put_group(VFIOGroup *group);
 int vfio_get_device(VFIOGroup *group, const char *name,
                     VFIODevice *vbasedev, Error **errp);
-int vfio_iommu_set_msi_binding(VFIOContainer *container, int n,
-                               IOMMUTLBEntry *entry);
-int vfio_iommu_unset_msi_binding(VFIOContainer *container, int n);
 
 extern const MemoryRegionOps vfio_region_ops;
 typedef QLIST_HEAD(VFIOGroupList, VFIOGroup) VFIOGroupList;
