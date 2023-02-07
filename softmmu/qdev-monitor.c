a88f2d12afb6a6b5b3d97983cea95d6088f0bf04
lixianglai <lixianglai@loongson.cn>
2023-02-07 07:21:17 -0500

    Add command line.
    Add loongarch command support.
    
    Signed-off-by: lixianglai <lixianglai@loongson.cn>

--- softmmu/qdev-monitor.c
+++ softmmu/qdev-monitor.c
@@ -62,7 +62,7 @@ typedef struct QDevAlias
                               QEMU_ARCH_MIPS | QEMU_ARCH_PPC |  \
                               QEMU_ARCH_RISCV | QEMU_ARCH_SH4 | \
                               QEMU_ARCH_SPARC | QEMU_ARCH_XTENSA | \
-			      QEMU_ARCH_SW64)
+                              QEMU_ARCH_SW64 | QEMU_ARCH_LOONGARCH64)
 #define QEMU_ARCH_VIRTIO_CCW (QEMU_ARCH_S390X)
 #define QEMU_ARCH_VIRTIO_MMIO (QEMU_ARCH_M68K)
 
