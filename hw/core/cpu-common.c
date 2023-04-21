openeuler-mirror/qemu

commit c3f86c199885506cfddde0dfc235c04e0897d591

arm/virt: Correct timing of executing cpu_synchronize_post_init for hot-plugged cpus
When the CPU starts normally, cpu_synchronize_post_init is executed
after GICv3 is implemented. This order should be followed when dealing
with hot-plugged CPUs.

Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>

hw/core/cpu-common.c
@@ -206,14 +206,12 @@ static void cpu_common_realizefn(DeviceState *dev, Error **errp)
         }
     }
 
+#ifdef __aarch64__
     if (dev->hotplugged) {
         cpu_synchronize_post_init(cpu);
-
-#ifdef __aarch64__
-    if (!kvm_enabled())
-#endif
         cpu_resume(cpu);
     }
+#endif
 
     /* NOTE: latest generic point where the cpu is fully realized */
     trace_init_vcpu(cpu);

hw/arm/virt.c
@@ -2798,6 +2798,7 @@ static void virt_cpu_plug(HotplugHandler *hotplug_dev,
         }
 
         /* Register CPU reset and trigger it manually */
+        cpu_synchronize_post_init(cs);
         cpu_synchronize_state(cs);
         cpu_hotplug_register_reset(ncpu);
         cpu_hotplug_reset_manually(ncpu);

commit a6f84e8f2c1fef8ca8f48f81e2e6a1b823d9a90e

core/cpu-common: Fix the wrong '#ifdef __aarch64__'
commit c3f86c199885 ("arm/virt: Correct timing of executing
cpu_synchronize_post_init for hot-plugged cpus") used the
wrong '#ifdef __aarch64__'. It should be '#ifndef __aarch64__'.

Fixes: c3f86c199885 ("arm/virt: Correct timing of executing
cpu_synchronize_post_init for hot-plugged cpus")
Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>

hw/core/cpu-common.c
@@ -206,7 +206,7 @@ static void cpu_common_realizefn(DeviceState *dev, Error **errp)
         }
     }
 
-#ifdef __aarch64__
+#ifndef __aarch64__
     if (dev->hotplugged) {
         cpu_synchronize_post_init(cpu);
         cpu_resume(cpu);