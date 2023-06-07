ba8973a75fffa55977dfa12a850827e11081280f
Hyman Huang(黄勇) <yong.huang@smartx.com>
2023-06-08 00:21:58 +0800

    migration: Extend query-migrate to provide dirty page limit info
    There is a conflict when merging patches in qemu-6.2.0, and there is a phenomenon that the corresponding files and functions cannot be found.Merge after modifying the patch by wangguochun@kylinos.cn
    
    Extend query-migrate to provide throttle time and estimated
    ring full time with dirty-limit capability enabled, through which
    we can observe if dirty limit take effect during live migration.
    
    Signed-off-by: Hyman Huang(黄勇) <yong.huang@smartx.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Message-ID: <168733225273.5845.15871826788879741674-8@git.sr.ht>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -61,6 +61,7 @@
 #include "sysemu/cpus.h"
 #include "yank_functions.h"
 #include "sysemu/kvm.h"
+#include "sysemu/dirtylimit.h"
 
 #define MAX_THROTTLE  (128 << 20)      /* Migration transfer speed throttling */
 
@@ -1059,6 +1060,15 @@ static void populate_ram_info(MigrationInfo *info, MigrationState *s)
     if (s->state != MIGRATION_STATUS_COMPLETED) {
         info->ram->remaining = ram_bytes_remaining();
         info->ram->dirty_pages_rate = ram_counters.dirty_pages_rate;
+    }
+
+    if (migrate_dirty_limit() && dirtylimit_in_service()) {
+        info->has_dirty_limit_throttle_time_per_round = true;
+        info->dirty_limit_throttle_time_per_round =
+                            dirtylimit_throttle_time_per_round();
+
+        info->has_dirty_limit_ring_full_time = true;
+        info->dirty_limit_ring_full_time = dirtylimit_ring_full_time();
     }
 }
 
