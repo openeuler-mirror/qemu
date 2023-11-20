b0a71ca260eedba4410b7c113bc04d364278c526
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-20 11:28:36 +0100

    memory: Have memory_region_init_ram() handler return a boolean
    commit fe5f33d6b086d6911d69dd77abe09b21bdb2ea5f upstream
    
    Following the example documented since commit e3fe3988d7 ("error:
    Document Error API usage rules"), have memory_region_init_ram()
    return a boolean indicating whether an error is set or not.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Message-Id: <20231120213301.24349-7-philmd@linaro.org>
    Signed-off-by: Jeevan deep J <j.jeevandeep@amd.com>
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- system/memory.c
+++ system/memory.c
@@ -3665,7 +3665,7 @@ void mtree_info(bool flatview, bool dispatch_tree, bool owner, bool disabled)
     }
 }
 
-void memory_region_init_ram(MemoryRegion *mr,
+bool memory_region_init_ram(MemoryRegion *mr,
                             Object *owner,
                             const char *name,
                             uint64_t size,
@@ -3674,7 +3674,7 @@ void memory_region_init_ram(MemoryRegion *mr,
     DeviceState *owner_dev;
 
     if (!memory_region_init_ram_nomigrate(mr, owner, name, size, errp)) {
-        return;
+        return false;
     }
     /* This will assert if owner is neither NULL nor a DeviceState.
      * We only want the owner here for the purposes of defining a
@@ -3684,6 +3684,8 @@ void memory_region_init_ram(MemoryRegion *mr,
      */
     owner_dev = DEVICE(owner);
     vmstate_register_ram(mr, owner_dev);
+
+    return true;
 }
 
 void memory_region_init_rom(MemoryRegion *mr,
