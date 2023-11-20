68ca7da00f3ec40cfb5d1cf65af0d5fb22067af2
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-20 11:30:34 +0100

    memory: Have memory_region_init_rom_device_nomigrate() return a boolean
    commit ae076b6c39bfd2027e35ca5be922dc8f1b18b901 upstream
    
    Following the example documented since commit e3fe3988d7
    ("error: Document Error API usage rules"), have
    memory_region_init_rom_device_nomigrate() return a boolean
    indicating whether an error is set or not.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Message-Id: <20231120213301.24349-9-philmd@linaro.org>
    Signed-off-by: Jeevan deep J <j.jeevandeep@amd.com>
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- system/memory.c
+++ system/memory.c
@@ -1749,7 +1749,7 @@ bool memory_region_init_rom_nomigrate(MemoryRegion *mr,
     return true;
 }
 
-void memory_region_init_rom_device_nomigrate(MemoryRegion *mr,
+bool memory_region_init_rom_device_nomigrate(MemoryRegion *mr,
                                              Object *owner,
                                              const MemoryRegionOps *ops,
                                              void *opaque,
@@ -1770,7 +1770,9 @@ void memory_region_init_rom_device_nomigrate(MemoryRegion *mr,
         mr->size = int128_zero();
         object_unparent(OBJECT(mr));
         error_propagate(errp, err);
+        return false;
     }
+    return true;
 }
 
 void memory_region_init_iommu(void *_iommu_mr,
