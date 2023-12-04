9cd596fd081bdb88b03b5e969631d8d08797c14d
fangyi <eric.fangyi@huawei.com>
2023-12-04 15:53:28 +0800

    vhost: implement savevm_hanlder for vdpa device
    Signed-off-by: libai <libai12@huawei.com>
    Signed-off-by: jiangdongxu <jiangdongxu1@huawei.com>
    Signed-off-by: fangyi <eric.fangyi@huawei.com>

--- include/hw/virtio/vdpa-dev-mig.h
+++ include/hw/virtio/vdpa-dev-mig.h
@@ -9,6 +9,19 @@
 
 #include "hw/virtio/vdpa-dev.h"
 
+enum {
+    VDPA_DEVICE_START,
+    VDPA_DEVICE_STOP,
+    VDPA_DEVICE_PRE_START,
+    VDPA_DEVICE_PRE_STOP,
+    VDPA_DEVICE_CANCEL,
+    VDPA_DEVICE_POST_START,
+    VDPA_DEVICE_START_ASYNC,
+    VDPA_DEVICE_STOP_ASYNC,
+    VDPA_DEVICE_PRE_START_ASYNC,
+    VDPA_DEVICE_QUERY_OP_STATE,
+};
+
 void vdpa_migration_register(VhostVdpaDevice *vdev);
 
 void vdpa_migration_unregister(VhostVdpaDevice *vdev);
