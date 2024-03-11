d29be82ad130b691d0cb2e03294f2fdf6d0c7653
Zhao Liu <zhao1.liu@intel.com>
2024-03-11 11:38:03 +0800

    block/snapshot: Fix missing ERRP_GUARD() for error_prepend()
    As the comment in qapi/error, passing @errp to error_prepend() requires
    ERRP_GUARD():
    
    * = Why, when and how to use ERRP_GUARD() =
    *
    * Without ERRP_GUARD(), use of the @errp parameter is restricted:
    ...
    * - It should not be passed to error_prepend(), error_vprepend() or
    *   error_append_hint(), because that doesn't work with &error_fatal.
    * ERRP_GUARD() lifts these restrictions.
    *
    * To use ERRP_GUARD(), add it right at the beginning of the function.
    * @errp can then be used without worrying about the argument being
    * NULL or &error_fatal.
    
    ERRP_GUARD() could avoid the case when @errp is &error_fatal, the user
    can't see this additional information, because exit() happens in
    error_setg earlier than information is added [1].
    
    In block/snapshot.c, there are 2 functions passing @errp to
    error_prepend() without ERRP_GUARD():
     - bdrv_all_delete_snapshot()
     - bdrv_all_goto_snapshot()
    
    As the APIs exposed in include/block/snapshot.h, they could be called
    by other modules.
    
    To avoid potential issues as [1] said, add missing ERRP_GUARD() at the
    beginning of these 2 functions.
    
    [1]: Issue description in the commit message of commit ae7c80a7bd73
         ("error: New macro ERRP_GUARD()").
    
    Cc: Kevin Wolf <kwolf@redhat.com>
    Cc: Hanna Reitz <hreitz@redhat.com>
    Cc: qemu-block@nongnu.org
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Message-ID: <20240311033822.3142585-11-zhao1.liu@linux.intel.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- block/snapshot.c
+++ block/snapshot.c
@@ -572,6 +572,7 @@ int bdrv_all_delete_snapshot(const char *name,
                              bool has_devices, strList *devices,
                              Error **errp)
 {
+    ERRP_GUARD();
     g_autoptr(GList) bdrvs = NULL;
     GList *iterbdrvs;
 
@@ -614,6 +615,7 @@ int bdrv_all_goto_snapshot(const char *name,
                            bool has_devices, strList *devices,
                            Error **errp)
 {
+    ERRP_GUARD();
     g_autoptr(GList) bdrvs = NULL;
     GList *iterbdrvs;
     int ret;
