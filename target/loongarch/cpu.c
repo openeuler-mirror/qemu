f572c385e0d368cbf12acf7d6f0b33b5f2efd7f0
Bibo Mao <maobibo@loongson.cn>
2024-05-21 16:05:48 +0800

    target/loongarch: Add loongarch vector property unconditionally
    Currently LSX/LASX vector property is decided by the default value.
    Instead vector property should be added unconditionally, and it is
    irrelative with its default value. If vector is disabled by default,
    vector also can be enabled from command line.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Reviewed-by: Song Gao <gaosong@loongson.cn>
    Message-Id: <20240521080549.434197-2-maobibo@loongson.cn>
    Signed-off-by: Song Gao <gaosong@loongson.cn>

--- target/loongarch/cpu.c
+++ target/loongarch/cpu.c
@@ -720,14 +720,10 @@ void loongarch_cpu_post_init(Object *obj)
 {
     LoongArchCPU *cpu = LOONGARCH_CPU(obj);
 
-    if (FIELD_EX32(cpu->env.cpucfg[2], CPUCFG2, LSX)) {
-        object_property_add_bool(obj, "lsx", loongarch_get_lsx,
-                                 loongarch_set_lsx);
-    }
-    if (FIELD_EX32(cpu->env.cpucfg[2], CPUCFG2, LASX)) {
-        object_property_add_bool(obj, "lasx", loongarch_get_lasx,
-                                 loongarch_set_lasx);
-    }
+    object_property_add_bool(obj, "lsx", loongarch_get_lsx,
+                             loongarch_set_lsx);
+    object_property_add_bool(obj, "lasx", loongarch_get_lasx,
+                             loongarch_set_lasx);
 
     if (kvm_enabled()) {
         object_property_add_bool(obj, "pmu", loongarch_get_pmu,
