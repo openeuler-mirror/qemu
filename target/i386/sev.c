8899b31177f4907fe5fd8d942431a9d338438856
Pankaj Gupta <pankaj.gupta@amd.com>
2024-05-30 06:16:18 -0500

    i386/sev: Move sev_launch_finish to separate class method
    commit bce615a14aec07cab0488e5a242f6a91e641efcb upstream.
    
    When sev-snp-guest objects are introduced there will be a number of
    differences in how the launch finish is handled compared to the existing
    sev-guest object. Move sev_launch_finish() to a class method to make it
    easier to implement SNP-specific launch update functionality later.
    
    [Backport Changes]
    
    In file target/i386/sev.c upstream commit changed sev_launch_finish()
    to take SevCommonState *sev_common instead of SevGuestState *sev_guest.
    
    Due to this change, sev_guest is no longer directly available in the
    function. The non-upstream CSV3 specific logic added in commit b9781c29e7a99
    requires access to SevGuestState fields such as host_data. To preserve the e
    xisting CSV3 behavior, SevGuestState is derived from sev_common using
    SEV_GUEST(sev_common) similar to upstream, inside the csv3_enabled() block
    before accessing sev_guest->host_data.
    
    This keeps the downstream CSV3 handling intact while adapting the code
    to the upstream function signature change.
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@amd.com>
    Message-ID: <20240530111643.1091816-7-pankaj.gupta@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Jeevan deep J <j.jeevandeep@amd.com>
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- target/i386/sev.c
+++ target/i386/sev.c
@@ -79,6 +79,7 @@ struct SevCommonStateClass {
 
     /* public */
     int (*launch_start)(SevCommonState *sev_common);
+    void (*launch_finish)(SevCommonState *sev_common);
 };
 
 /**
@@ -952,19 +953,21 @@ static Notifier sev_machine_done_notify = {
 };
 
 static void
-sev_launch_finish(SevGuestState *sev_guest)
+sev_launch_finish(SevCommonState *sev_common)
 {
     int ret, error;
 
-    if (csv3_enabled() &&
-        sev_guest->host_data &&
-        (kvm_hygon_coco_ext_inuse & KVM_CAP_HYGON_COCO_EXT_CSV3_LFINISH_EX)) {
-         csv3_launch_finish_ex(sev_guest->host_data);
-         goto common_finish;
+    if (csv3_enabled()) {
+        SevGuestState *sev_guest = SEV_GUEST(sev_common);
+        if (sev_guest->host_data &&
+           (kvm_hygon_coco_ext_inuse & KVM_CAP_HYGON_COCO_EXT_CSV3_LFINISH_EX)) {
+            csv3_launch_finish_ex(sev_guest->host_data);
+            goto common_finish;
+        }
     }
 
     trace_kvm_sev_launch_finish();
-    ret = sev_ioctl(SEV_COMMON(sev_guest)->sev_fd, KVM_SEV_LAUNCH_FINISH, 0,
+    ret = sev_ioctl(sev_common->sev_fd, KVM_SEV_LAUNCH_FINISH, 0,
                     &error);
     if (ret) {
         error_report("%s: LAUNCH_FINISH ret=%d fw_error=%d '%s'",
@@ -973,7 +976,7 @@ sev_launch_finish(SevGuestState *sev_guest)
     }
 
 common_finish:
-    sev_set_guest_state(SEV_COMMON(sev_guest), SEV_STATE_RUNNING);
+    sev_set_guest_state(sev_common, SEV_STATE_RUNNING);
 
     /* add migration blocker */
     error_setg(&sev_mig_blocker,
@@ -1009,12 +1012,13 @@ static void
 sev_vm_state_change(void *opaque, bool running, RunState state)
 {
     SevCommonState *sev_common = opaque;
+    SevCommonStateClass *klass = SEV_COMMON_GET_CLASS(opaque);
 
     if (running) {
         if (sev_check_state(sev_common, SEV_STATE_RECEIVE_UPDATE)) {
             sev_receive_finish(sev_common);
         } else if (!sev_check_state(sev_common, SEV_STATE_RUNNING)) {
-            sev_launch_finish(SEV_GUEST(sev_common));
+            klass->launch_finish(sev_common);
         }
     }
 }
@@ -2915,6 +2919,7 @@ sev_guest_class_init(ObjectClass *oc, void *data)
     SevCommonStateClass *klass = SEV_COMMON_CLASS(oc);
 
     klass->launch_start = sev_launch_start;
+    klass->launch_finish = sev_launch_finish;
 
     object_class_property_add_str(oc, "dh-cert-file",
                                   sev_guest_get_dh_cert_file,
