e711253e77b249686e5a4d3c15d474303de0cc99
Paolo Bonzini <pbonzini@redhat.com>
2024-07-03 13:42:49 +0200

    target/i386: add avx-vnni-int16 feature
    commit 138c3377a9b27accec516b2c0da90dedef98a780 upstream.
    
    AVX-VNNI-INT16 (CPUID[EAX=7,ECX=1).EDX[10]) is supported by Clearwater
    Forest processor, add it to QEMU as it does not need any specific
    enablement.
    
    Intel-SIG: commit 138c3377a9b2 target/i386: add avx-vnni-int16 feature.
    CWF ISA AVX_VNNI_INT16 backporting
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -987,7 +987,7 @@ FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
         .feat_names = {
             NULL, NULL, NULL, NULL,
             "avx-vnni-int8", "avx-ne-convert", NULL, NULL,
-            "amx-complex", NULL, NULL, NULL,
+            "amx-complex", NULL, "avx-vnni-int16", NULL,
             NULL, NULL, "prefetchiti", NULL,
             NULL, NULL, NULL, "avx10",
             NULL, NULL, NULL, NULL,
