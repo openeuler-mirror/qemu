f622157a79ca2f7e45693ab32fa1a8114cdf3e54
Mattias Nissler <mnissler@rivosinc.com>
2024-08-19 06:54:54 -0700

    softmmu: Support concurrent bounce buffers(CVE-2024-8612)
    cherry-pick from 637b0aa139565cb82a7b9269e62214f87082635c
    
    When DMA memory can't be directly accessed, as is the case when
    running the device model in a separate process without shareable DMA
    file descriptors, bounce buffering is used.
    
    It is not uncommon for device models to request mapping of several DMA
    regions at the same time. Examples include:
     * net devices, e.g. when transmitting a packet that is split across
       several TX descriptors (observed with igb)
     * USB host controllers, when handling a packet with multiple data TRBs
       (observed with xhci)
    
    Previously, qemu only provided a single bounce buffer per AddressSpace
    and would fail DMA map requests while the buffer was already in use. In
    turn, this would cause DMA failures that ultimately manifest as hardware
    errors from the guest perspective.
    
    This change allocates DMA bounce buffers dynamically instead of
    supporting only a single buffer. Thus, multiple DMA mappings work
    correctly also when RAM can't be mmap()-ed.
    
    The total bounce buffer allocation size is limited individually for each
    AddressSpace. The default limit is 4096 bytes, matching the previous
    maximum buffer size. A new x-max-bounce-buffer-size parameter is
    provided to configure the limit for PCI devices.
    
    Signed-off-by: Mattias Nissler <mnissler@rivosinc.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Acked-by: Peter Xu <peterx@redhat.com>
    Link: https://lore.kernel.org/r/20240819135455.2957406-1-mnissler@rivosinc.com
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -71,6 +71,8 @@ static Property pci_props[] = {
                     QEMU_PCIE_LNKSTA_DLLLA_BITNR, true),
     DEFINE_PROP_BIT("x-pcie-extcap-init", PCIDevice, cap_present,
                     QEMU_PCIE_EXTCAP_INIT_BITNR, true),
+    DEFINE_PROP_SIZE32("x-max-bounce-buffer-size", PCIDevice,
+                     max_bounce_buffer_size, DEFAULT_MAX_BOUNCE_BUFFER_SIZE),
     DEFINE_PROP_END_OF_LIST()
 };
 
@@ -1051,6 +1053,8 @@ static PCIDevice *do_pci_register_device(PCIDevice *pci_dev,
                        "bus master container", UINT64_MAX);
     address_space_init(&pci_dev->bus_master_as,
                        &pci_dev->bus_master_container_region, pci_dev->name);
+    pci_dev->bus_master_as.max_bounce_buffer_size =
+        pci_dev->max_bounce_buffer_size;
 
     if (qdev_hotplug) {
         pci_init_bus_master(pci_dev);
@@ -2600,6 +2604,10 @@ static void pci_device_class_init(ObjectClass *klass, void *data)
     k->unrealize = pci_qdev_unrealize;
     k->bus_type = TYPE_PCI_BUS;
     k->props = pci_props;
+    object_class_property_set_description(
+        klass, "x-max-bounce-buffer-size",
+        "Maximum buffer size allocated for bounce buffers used for mapped "
+        "access to indirect DMA memory", NULL);
 }
 
 static void pci_device_class_base_init(ObjectClass *klass, void *data)
