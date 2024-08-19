openeuler-mirror/qemu

commit 5db954cb188d3775aec053fad8a39bf4c26a2b92

Add support for the virtcca cvm feature.
With this commit,we can append new startup parameters :"cma=64M
cvm_guest=1" and "kvm_type=cvm" to use virtcca cvm feature.
Here is a full example of the append parameters for a cvm :
-M virt,gic-version=3,accel=kvm,kernel_irqchip=on,kvm_type=cvm \
-append "swiotlb=force console=tty0 console=ttyAMA0 kaslr.disabled=1
cma=64M cvm_guest=1 rodata=off rootfstype=ext4 root=/dev/vad rw" \

Additionally,the SVE and PMU are optional configurations for cvm,here is
an example:
-object tmm-guest,id=tmm0,sve-vector-length=128,num-pmu-counters=1

Signed-off-by: liupingwei <liupingwei0317@outlook.com>

hw/virtio/virtio-bus.c
@@ -25,6 +25,7 @@
 #include "qemu/osdep.h"
 #include "qemu/error-report.h"
 #include "qemu/module.h"
+#include "sysemu/kvm.h"
 #include "qapi/error.h"
 #include "hw/virtio/virtio-bus.h"
 #include "hw/virtio/virtio.h"
@@ -81,6 +82,11 @@ void virtio_bus_device_plugged(VirtIODevice *vdev, Error **errp)
     vdev->dma_as = &address_space_memory;
     if (has_iommu) {
         vdev_has_iommu = virtio_host_has_feature(vdev, VIRTIO_F_IOMMU_PLATFORM);
+
+        if (virtcca_cvm_enabled() && (strcmp(vdev->name, "vhost-user-fs") == 0)) {
+            vdev_has_iommu = true;
+        }
+
         /*
          * Present IOMMU_PLATFORM to the driver iff iommu_plattform=on and
          * device operational. If the driver does not accept IOMMU_PLATFORM

accel/kvm/kvm-all.c
@@ -52,6 +52,8 @@
 #include "hw/boards.h"
 #include "sysemu/stats.h"
 
+#include "sysemu/kvm.h"
+
 /* This check must be after config-host.h is included */
 #ifdef CONFIG_EVENTFD
 #include <sys/eventfd.h>
@@ -86,6 +88,9 @@ struct KVMParkedVcpu {
 };
 
 KVMState *kvm_state;
+
+bool virtcca_cvm_allowed = false;
+
 bool kvm_kernel_irqchip;
 bool kvm_split_irqchip;
 bool kvm_async_interrupts_allowed;
@@ -2355,6 +2360,11 @@ uint32_t kvm_dirty_ring_size(void)
     return kvm_state->kvm_dirty_ring_size;
 }
 
+static inline bool kvm_is_virtcca_cvm_type(int type)
+{
+    return type & VIRTCCA_CVM_TYPE;
+}
+
 static int kvm_init(MachineState *ms)
 {
     MachineClass *mc = MACHINE_GET_CLASS(ms);
@@ -2445,6 +2455,10 @@ static int kvm_init(MachineState *ms)
     if (type < 0) {
         ret = -EINVAL;
         goto err;
+    }
+
+    if (kvm_is_virtcca_cvm_type(type)) {
+        virtcca_cvm_allowed = true;
     }
 
     do {
@@ -3501,6 +3515,28 @@ int kvm_get_one_reg(CPUState *cs, uint64_t id, void *target)
         trace_kvm_failed_reg_get(id, strerror(-r));
     }
     return r;
+}
+
+int kvm_load_user_data(hwaddr loader_start, hwaddr image_end, hwaddr initrd_start, hwaddr dtb_end, hwaddr ram_size,
+                       struct kvm_numa_info *numa_info)
+{
+    KVMState *state = kvm_state;
+    struct kvm_user_data data;
+    int ret;
+
+    data.loader_start = loader_start;
+    data.image_end = image_end;
+    data.initrd_start = initrd_start;
+    data.dtb_end = dtb_end;
+    data.ram_size = ram_size;
+    memcpy(&data.numa_info, numa_info, sizeof(struct kvm_numa_info));
+
+    ret = kvm_vm_ioctl(state, KVM_LOAD_USER_DATA, &data);
+    if (ret < 0) {
+        error_report("%s: KVM_LOAD_USER_DATA failed!\n", __func__);
+    }
+
+    return ret;
 }
 
 static bool kvm_accel_has_memory(MachineState *ms, AddressSpace *as,

hw/arm/boot.c
@@ -27,6 +27,7 @@
 #include "qemu/config-file.h"
 #include "qemu/option.h"
 #include "qemu/units.h"
+#include "kvm_arm.h"
 
 /* Kernel boot protocol is specified in the kernel docs
  * Documentation/arm/Booting and Documentation/arm64/booting.txt
@@ -1142,6 +1143,16 @@ static void arm_setup_direct_kernel_boot(ARMCPU *cpu,
     for (cs = first_cpu; cs; cs = CPU_NEXT(cs)) {
         ARM_CPU(cs)->env.boot_info = info;
     }
+
+    if (kvm_enabled() && virtcca_cvm_enabled()) {
+        if (info->dtb_limit == 0) {
+            info->dtb_limit = info->dtb_start + 0x200000;
+        }
+        kvm_load_user_data(info->loader_start, image_high_addr, info->initrd_start,
+                           info->dtb_limit, info->ram_size, (struct kvm_numa_info *)info->numa_info);
+        tmm_add_ram_region(info->loader_start, image_high_addr - info->loader_start,
+                           info->initrd_start, info->dtb_limit - info->initrd_start, true);
+    }
 }
 
 static void arm_setup_firmware_boot(ARMCPU *cpu, struct arm_boot_info *info)
@@ -1235,12 +1246,50 @@ void arm_load_kernel(ARMCPU *cpu, MachineState *ms, struct arm_boot_info *info)
     info->initrd_filename = ms->initrd_filename;
     info->dtb_filename = ms->dtb;
     info->dtb_limit = 0;
+    if (kvm_enabled() && virtcca_cvm_enabled()) {
+        info->ram_size = ms->ram_size;
+        info->numa_info = g_malloc(sizeof(struct kvm_numa_info));
+        struct kvm_numa_info *numa_info = (struct kvm_numa_info *) info->numa_info;
+        if (ms->numa_state != NULL && ms->numa_state->num_nodes > 0) {
+            numa_info->numa_cnt = ms->numa_state->num_nodes;
+            uint64_t mem_base = info->loader_start;
+            for (int64_t i = 0; i < ms->numa_state->num_nodes && i < MAX_NUMA_NODE; i++) {
+                uint64_t mem_len = ms->numa_state->nodes[i].node_mem;
+                numa_info->numa_nodes[i].numa_id = i;
+                numa_info->numa_nodes[i].ipa_start = mem_base;
+                numa_info->numa_nodes[i].ipa_size = mem_len;
+                memcpy(numa_info->numa_nodes[i].host_numa_nodes, ms->numa_state->nodes[i].node_memdev->host_nodes,
+                       MAX_NODES / BITS_PER_LONG * sizeof(uint64_t));
+                mem_base += mem_len;
+            }
+        } else {
+            numa_info->numa_cnt = 1;
+            numa_info->numa_nodes[0].numa_id = 0;
+            numa_info->numa_nodes[0].ipa_start = info->loader_start;
+            numa_info->numa_nodes[0].ipa_size = info->ram_size;
+            memset(numa_info->numa_nodes[0].host_numa_nodes, 0, MAX_NODES / BITS_PER_LONG * sizeof(uint64_t));
+        }
+
+        for (int cpu_idx = ms->smp.cpus - 1; cpu_idx >= 0; cpu_idx--) {
+            ARMCPU *armcpu = ARM_CPU(qemu_get_cpu(cpu_idx));
+            CPUState *local_cs = CPU(armcpu);
+            uint64_t node_id = 0;
+            if (ms->possible_cpus->cpus[local_cs->cpu_index].props.has_node_id)
+                node_id = ms->possible_cpus->cpus[local_cs->cpu_index].props.node_id;
+            bitmap_set((unsigned long *)numa_info->numa_nodes[node_id].cpu_id, cpu_idx, 1);
+        }
+    }
 
     /* Load the kernel.  */
     if (!info->kernel_filename || info->firmware_loaded) {
         arm_setup_firmware_boot(cpu, info);
     } else {
         arm_setup_direct_kernel_boot(cpu, info);
+    }
+
+    if (kvm_enabled() && virtcca_cvm_enabled()) {
+        g_free(info->numa_info);
+        info->numa_info = NULL;
     }
 
     /*

hw/arm/virt.c
@@ -285,8 +285,16 @@ static void create_fdt(VirtMachineState *vms)
 
     /* /chosen must exist for load_dtb to fill in necessary properties later */
     qemu_fdt_add_subnode(fdt, "/chosen");
+
+    g_autofree char *kvm_type = NULL;
+    if (object_property_find(OBJECT(current_machine), "kvm-type")) {
+        kvm_type = object_property_get_str(OBJECT(current_machine),
+                                           "kvm-type", &error_abort);
+    }
     if (vms->dtb_randomness) {
-        create_randomness(ms, "/chosen");
+        if (!(kvm_type && !strcmp(kvm_type, "cvm"))) {
+            create_randomness(ms, "/chosen");
+        }
     }
 
     if (vms->secure) {
@@ -1953,6 +1961,19 @@ static void virt_set_memmap(VirtMachineState *vms, int pa_bits)
         vms->memmap[i] = base_memmap[i];
     }
 
+    /* fix VIRT_MEM range */
+    if (object_property_find(OBJECT(current_machine), "kvm-type")) {
+        g_autofree char *kvm_type = object_property_get_str(OBJECT(current_machine),
+                                                            "kvm-type", &error_abort);
+
+        if (!strcmp(kvm_type, "cvm")) {
+            vms->memmap[VIRT_MEM].base = 3 * GiB;
+            vms->memmap[VIRT_MEM].size = ms->ram_size;
+            info_report("[qemu] fix VIRT_MEM range 0x%llx - 0x%llx\n", (unsigned long long)(vms->memmap[VIRT_MEM].base),
+                 (unsigned long long)(vms->memmap[VIRT_MEM].base + ms->ram_size));
+        }
+    }
+
     if (ms->ram_slots > ACPI_MAX_RAM_SLOTS) {
         error_report("unsupported number of memory slots: %"PRIu64,
                      ms->ram_slots);
@@ -2440,7 +2461,7 @@ static void machvirt_init(MachineState *machine)
      */
     if (vms->secure && firmware_loaded) {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_DISABLED;
-    } else if (vms->virt) {
+    } else if (vms->virt || virtcca_cvm_enabled()) {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_SMC;
     } else {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_HVC;
@@ -2509,6 +2530,13 @@ static void machvirt_init(MachineState *machine)
         }
     }
 
+    if (virtcca_cvm_enabled()) {
+        int ret = kvm_arm_tmm_init(machine->cgs, &error_fatal);
+        if (ret != 0) {
+            error_report("fail to initialize TMM");
+            exit(1);
+        }
+    }
     create_fdt(vms);
     qemu_log("cpu init start\n");
 
@@ -3592,6 +3620,15 @@ static HotplugHandler *virt_machine_get_hotplug_handler(MachineState *machine,
 static int virt_kvm_type(MachineState *ms, const char *type_str)
 {
     VirtMachineState *vms = VIRT_MACHINE(ms);
+    int virtcca_cvm_type = 0;
+    if (object_property_find(OBJECT(current_machine), "kvm-type")) {
+        g_autofree char *kvm_type = object_property_get_str(OBJECT(current_machine),
+                                                            "kvm-type", &error_abort);
+
+        if (!strcmp(kvm_type, "cvm")) {
+            virtcca_cvm_type = VIRTCCA_CVM_TYPE;
+        }
+    }
     int max_vm_pa_size, requested_pa_size;
     bool fixed_ipa;
 
@@ -3621,7 +3658,9 @@ static int virt_kvm_type(MachineState *ms, const char *type_str)
      * the implicit legacy 40b IPA setting, in which case the kvm_type
      * must be 0.
      */
-    return fixed_ipa ? 0 : requested_pa_size;
+    return strcmp(type_str, "cvm") == 0 ?
+        ((fixed_ipa ? 0 : requested_pa_size) | virtcca_cvm_type) :
+        (fixed_ipa ? 0 : requested_pa_size);
 }
 
 static void virt_machine_class_init(ObjectClass *oc, void *data)
@@ -3793,6 +3832,19 @@ static void virt_machine_class_init(ObjectClass *oc, void *data)
 
 }
 
+static char *virt_get_kvm_type(Object *obj, Error **errp G_GNUC_UNUSED)
+{
+    VirtMachineState *vms = VIRT_MACHINE(obj);
+    return g_strdup(vms->kvm_type);
+}
+
+static void virt_set_kvm_type(Object *obj, const char *value, Error **errp G_GNUC_UNUSED)
+{
+    VirtMachineState *vms = VIRT_MACHINE(obj);
+    g_free(vms->kvm_type);
+    vms->kvm_type = g_strdup(value);
+}
+
 static void virt_instance_init(Object *obj)
 {
     VirtMachineState *vms = VIRT_MACHINE(obj);
@@ -3853,6 +3905,9 @@ static void virt_instance_init(Object *obj)
 
     vms->oem_id = g_strndup(ACPI_BUILD_APPNAME6, 6);
     vms->oem_table_id = g_strndup(ACPI_BUILD_APPNAME8, 8);
+
+    object_property_add_str(obj, "kvm-type", virt_get_kvm_type, virt_set_kvm_type);
+    object_property_set_description(obj, "kvm-type", "CVM or Normal VM");
 }
 
 static const TypeInfo virt_machine_info = {

include/hw/arm/boot.h
@@ -39,6 +39,7 @@ void armv7m_load_kernel(ARMCPU *cpu, const char *kernel_filename,
 /* arm_boot.c */
 struct arm_boot_info {
     uint64_t ram_size;
+    void *numa_info;
     const char *kernel_filename;
     const char *kernel_cmdline;
     const char *initrd_filename;

include/hw/arm/virt.h
@@ -182,6 +182,7 @@ struct VirtMachineState {
     PCIBus *bus;
     char *oem_id;
     char *oem_table_id;
+    char *kvm_type;
     NotifierList cpuhp_notifiers;
 };
 

include/sysemu/kvm.h
@@ -19,6 +19,7 @@
 #include "exec/memattrs.h"
 #include "qemu/accel.h"
 #include "qom/object.h"
+#include "linux-headers/linux/kvm.h"
 
 #ifdef NEED_CPU_H
 # ifdef CONFIG_KVM
@@ -32,6 +33,7 @@
 #ifdef CONFIG_KVM_IS_POSSIBLE
 
 extern bool kvm_allowed;
+extern bool virtcca_cvm_allowed;
 extern bool kvm_kernel_irqchip;
 extern bool kvm_split_irqchip;
 extern bool kvm_async_interrupts_allowed;
@@ -44,6 +46,8 @@ extern bool kvm_readonly_mem_allowed;
 extern bool kvm_msi_use_devid;
 
 #define kvm_enabled()           (kvm_allowed)
+#define virtcca_cvm_enabled()           (virtcca_cvm_allowed)
+#define VIRTCCA_CVM_TYPE (1UL << 8)
 /**
  * kvm_irqchip_in_kernel:
  *
@@ -146,6 +150,8 @@ extern bool kvm_msi_use_devid;
 #else
 
 #define kvm_enabled()           (0)
+#define virtcca_cvm_enabled()           (0)
+#define VIRTCCA_CVM_TYPE        (0)
 #define kvm_irqchip_in_kernel() (false)
 #define kvm_irqchip_is_split() (false)
 #define kvm_async_interrupts_enabled() (false)
@@ -542,6 +548,9 @@ bool kvm_arch_cpu_check_are_resettable(void);
 bool kvm_dirty_ring_enabled(void);
 
 uint32_t kvm_dirty_ring_size(void);
+
+int kvm_load_user_data(hwaddr loader_start, hwaddr image_end, hwaddr initrd_start, hwaddr dtb_end, hwaddr ram_size,
+                       struct kvm_numa_info *numa_info);
 
 #ifdef __aarch64__
 int kvm_create_shadow_device(PCIDevice *dev);

linux-headers/asm-arm64/kvm.h
@@ -110,6 +110,7 @@ struct kvm_regs {
 #define KVM_ARM_VCPU_PTRAUTH_ADDRESS	5 /* VCPU uses address authentication */
 #define KVM_ARM_VCPU_PTRAUTH_GENERIC	6 /* VCPU uses generic authentication */
 #define KVM_ARM_VCPU_HAS_EL2		7 /* Support nested virtualization */
+#define KVM_ARM_VCPU_TEC		8 /* VCPU TEC state as part of cvm */
 
 struct kvm_vcpu_init {
 	__u32 target;
@@ -521,6 +522,67 @@ struct reg_mask_range {
 	__u64 addr;		/* Pointer to mask array */
 	__u32 range;		/* Requested range */
 	__u32 reserved[13];
+};
+
+/* KVM_CAP_ARM_TMM on VM fd */
+#define KVM_CAP_ARM_TMM_CONFIG_CVM		0
+#define KVM_CAP_ARM_TMM_CREATE_RD		1
+#define KVM_CAP_ARM_TMM_POPULATE_CVM		2
+#define KVM_CAP_ARM_TMM_ACTIVATE_CVM		3
+ 
+#define KVM_CAP_ARM_TMM_MEASUREMENT_ALGO_SHA256		0
+#define KVM_CAP_ARM_TMM_MEASUREMENT_ALGO_SHA512		1
+ 
+#define KVM_CAP_ARM_TMM_RPV_SIZE 64
+ 
+/* List of configuration items accepted for KVM_CAP_ARM_RME_CONFIG_REALM */
+#define KVM_CAP_ARM_TMM_CFG_RPV			0
+#define KVM_CAP_ARM_TMM_CFG_HASH_ALGO		1
+#define KVM_CAP_ARM_TMM_CFG_SVE			2
+#define KVM_CAP_ARM_TMM_CFG_DBG			3
+#define KVM_CAP_ARM_TMM_CFG_PMU			4
+
+struct kvm_cap_arm_tmm_config_item {
+	__u32 cfg;
+	union {
+		/* cfg == KVM_CAP_ARM_TMM_CFG_RPV */
+		struct {
+			__u8 rpv[KVM_CAP_ARM_TMM_RPV_SIZE];
+		};
+
+		/* cfg == KVM_CAP_ARM_TMM_CFG_HASH_ALGO */
+		struct {
+			__u32 hash_algo;
+		};
+
+		/* cfg == KVM_CAP_ARM_TMM_CFG_SVE */
+		struct {
+			__u32 sve_vq;
+		};
+
+		/* cfg == KVM_CAP_ARM_TMM_CFG_DBG */
+		struct {
+			__u32 num_brps;
+			__u32 num_wrps;
+		};
+
+		/* cfg == KVM_CAP_ARM_TMM_CFG_PMU */
+		struct {
+			__u32 num_pmu_cntrs;
+		};
+		/* Fix the size of the union */
+		__u8 reserved[256];
+	};
+};
+
+#define KVM_ARM_TMM_POPULATE_FLAGS_MEASURE	(1U << 0)
+struct kvm_cap_arm_tmm_populate_region_args {
+	__u64 populate_ipa_base1;
+	__u64 populate_ipa_size1;
+	__u64 populate_ipa_base2;
+	__u64 populate_ipa_size2;
+	__u32 flags;
+	__u32 reserved[3];
 };
 
 #endif

commit 282d63f9b5915f0529e9d0ae54b47c0ceacc58c3

cvm : bug-fix for incorrect device name check for vhost-user-fs
The 'vhost-user-fs' was being parsed as 'virtio-user-fs' during the
compilation and this caused the device to erroneously trigger the error
branch.

Fixes: 5db954cb188d3775aec053fad8a39bf4c26a2b92("Add support for the
virtcca cvm feature.)

Signed-off-by: liupingwei <liupingwei0317@outlook.com>

hw/virtio/virtio-bus.c
@@ -83,7 +83,7 @@ void virtio_bus_device_plugged(VirtIODevice *vdev, Error **errp)
     if (has_iommu) {
         vdev_has_iommu = virtio_host_has_feature(vdev, VIRTIO_F_IOMMU_PLATFORM);
 
-        if (virtcca_cvm_enabled() && (strcmp(vdev->name, "vhost-user-fs") == 0)) {
+        if (virtcca_cvm_enabled() && (strcmp(vdev->name, "virtio-user-fs") == 0)) {
             vdev_has_iommu = true;
         }
 