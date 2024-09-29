fe8e83c0695b8215a3eab44eb4e40e4c4b90b2d8
刘婧20201110 <liujing_yewu@cmss.chinamobile.com>
2024-09-29 17:25:23 +0800

    target/ppc: Remove unused xer_* macros
    The macros xer_ov, xer_ca, xer_ov32, and xer_ca32 are both unused and
    hiding the usage of env. Remove them.
    
    Signed-off-by: Víctor Colombo <victor.colombo@eldorado.org.br>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20220906125523.38765-3-victor.colombo@eldorado.org.br>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Signed-off-by: Liu Jing <liujing@cmss.chinamobile.com>

--- target/ppc/cpu.h
+++ target/ppc/cpu.h
@@ -1448,10 +1448,6 @@ typedef PowerPCCPU ArchCPU;
 #define XER_CMP  8
 #define XER_BC   0
 #define xer_so  (env->so)
-#define xer_ov  (env->ov)
-#define xer_ca  (env->ca)
-#define xer_ov32  (env->ov)
-#define xer_ca32  (env->ca)
 #define xer_cmp ((env->xer >> XER_CMP) & 0xFF)
 #define xer_bc  ((env->xer >> XER_BC)  & 0x7F)
 
