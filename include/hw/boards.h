7eecea50279047dc77a516b5e7d3bce046fa06fa
Zhao Liu <zhao1.liu@intel.com>
2024-11-01 16:33:26 +0800

    hw/core: Check smp cache topology support for machine
    commit f35c0221fef864e65db7641bb041c5f913e31475 upstream.
    
    Add cache_supported flags in SMPCompatProps to allow machines to
    configure various caches support.
    
    And check the compatibility of the cache properties with the
    machine support in machine_parse_smp_cache().
    
    Intel-SIG: commit f35c0221fef8 hw/core: Check smp cache topology support for machine.
    CPU new topology backporting
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Tested-by: Yongwei Ma <yongwei.ma@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Message-ID: <20241101083331.340178-5-zhao1.liu@intel.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- include/hw/boards.h
+++ include/hw/boards.h
@@ -145,6 +145,8 @@ typedef struct {
  * @books_supported - whether books are supported by the machine
  * @drawers_supported - whether drawers are supported by the machine
  * @modules_supported - whether modules are supported by the machine
+ * @cache_supported - whether cache (l1d, l1i, l2 and l3) configuration are
+ *                    supported by the machine
  */
 typedef struct {
     bool prefer_sockets;
@@ -154,6 +156,7 @@ typedef struct {
     bool books_supported;
     bool drawers_supported;
     bool modules_supported;
+    bool cache_supported[CACHE_LEVEL_AND_TYPE__MAX];
 } SMPCompatProps;
 
 /**
