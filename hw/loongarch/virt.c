495febb2311485aab8ad0aa3af0675ac6951ae74
Bibo Mao <maobibo@loongson.cn>
2025-03-20 11:21:58 +0800

    target/loongarch: Clean up virt_cpu_irq_init() error handling
    The Error ** argument must be NULL, &error_abort, &error_fatal, or a
    pointer to a variable containing NULL. Passing an argument of the
    latter kind twice without clearing it in between is wrong: if the
    first call sets an error, it no longer points to NULL for the second
    call.
    
    virt_cpu_irq_init() is wrong that way: it passes &err to
    hotplug_handler_plug() twice.  If both calls failed, this could trip
    error_setv()'s assertion.  Moreover, if just one fails, the Error
    object leaks. Fortunately, these calls can't actually fail.
    
    Messed up in commit 50ebc3fc47f7 (hw/intc/loongarch_ipi: Notify ipi
    object when cpu is plugged) and commit 087a23a87c57
    (hw/intc/loongarch_extioi: Use cpu plug notification).
    
    Clean this up by passing &error_abort instead.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Acked-by: Markus Armbruster <armbru@redhat.com>
    Message-ID: <20250320032158.1762751-7-maobibo@loongson.cn>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>

--- hw/loongarch/virt.c
+++ hw/loongarch/virt.c
@@ -329,7 +329,6 @@ static void virt_cpu_irq_init(LoongArchVirtMachineState *lvms)
     MachineClass *mc = MACHINE_GET_CLASS(ms);
     const CPUArchIdList *possible_cpus;
     CPUState *cs;
-    Error *err = NULL;
 
     /* cpu nodes */
     possible_cpus = mc->possible_cpu_arch_ids(ms);
@@ -339,8 +338,10 @@ static void virt_cpu_irq_init(LoongArchVirtMachineState *lvms)
             continue;
         }
 
-        hotplug_handler_plug(HOTPLUG_HANDLER(lvms->ipi), DEVICE(cs), &err);
-        hotplug_handler_plug(HOTPLUG_HANDLER(lvms->extioi), DEVICE(cs), &err);
+        hotplug_handler_plug(HOTPLUG_HANDLER(lvms->ipi), DEVICE(cs),
+                             &error_abort);
+        hotplug_handler_plug(HOTPLUG_HANDLER(lvms->extioi), DEVICE(cs),
+                             &error_abort);
     }
 }
 
