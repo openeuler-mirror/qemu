4a065d0fbbe159dfbc073e4480434d6889b7c5a4
caijian <caijian11@h-partners.com>
2025-03-31 15:03:02 +0800

    tests/data/acpi: Update DSDT acpi tables
    - * Disassembly of tests/data/acpi/virt/DSDT, Fri Mar 28 16:43:04 2025
    + * Disassembly of /tmp/aml-1KF432, Fri Mar 28 16:43:04 2025
      *
      * Original Table Header:
      *     Signature        "DSDT"
      *     Length           0x000016B6 (5814)
      *     Revision         0x02
    - *     Checksum         0x46
    + *     Checksum         0x47
      *     OEM ID           "BOCHS "
      *     OEM Table ID     "BXPC    "
      *     OEM Revision     0x00000001 (1)
      *     Compiler ID      "BXPC"
      *     Compiler Version 0x00000001 (1)
      */
     DefinitionBlock ("", "DSDT", 2, "BOCHS ", "BXPC    ", 0x00000001)
    @@ -2090,33 +2090,33 @@
                     }
                     Else
                     {
                         CDW1 |= 0x04
                         Return (Arg3)
                     }
                 }
    
                 Method (_DSM, 4, NotSerialized)  // _DSM: Device-Specific Method
                 {
                     If ((Arg0 == ToUUID ("e5c937d0-3553-4d7a-9117-ea4d19c3434d") /* Device Labeling Interface */))
                     {
                         If ((Arg2 == Zero))
                         {
                             Return (Buffer (One)
                             {
    -                             0x01                                             // .
    +                             0x00                                             // .
                             })
                         }
                     }
    
                     Return (Buffer (One)
                     {
                          0x00
                     })
                 }
    
    Signed-off-by: caijian <caijian11@h-partners.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,7 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/microvm/DSDT.pcie",
-"tests/data/acpi/virt/DSDT",
-"tests/data/acpi/virt/DSDT.acpihmatvirt",
-"tests/data/acpi/virt/DSDT.memhp",
-"tests/data/acpi/virt/DSDT.pxb",
-"tests/data/acpi/virt/DSDT.topology",
