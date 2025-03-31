ea23e4215b332446d4964769d004f7a11caba00b
caijian <caijian11@h-partners.com>
2025-03-31 15:02:37 +0800

    tests/qtest: Allow DSDT acpi tables to change
    List all DSDT files and allow them to change.
    
    Signed-of-by: caijian <caijian11@h-partners.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,7 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/microvm/DSDT.pcie",
+"tests/data/acpi/virt/DSDT",
+"tests/data/acpi/virt/DSDT.acpihmatvirt",
+"tests/data/acpi/virt/DSDT.memhp",
+"tests/data/acpi/virt/DSDT.pxb",
+"tests/data/acpi/virt/DSDT.topology",
