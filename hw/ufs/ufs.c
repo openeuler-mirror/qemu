068fef175047c18f60900dacd54c7a436114c164
qihao_yewu <qihao_yewu@cmss.chinamobile.com>
2025-04-07 13:18:47 -0400

    hw/ufs: free irq on exit
    cheery-pick from c458f9474d6574505ce9144ab1a90b951e69c1bd
    
    Fix a memory leak bug in ufs_init_pci() due to u->irq
    not being freed in ufs_exit().
    
    Signed-off-by: Zheng Huang <hz1624917200@gmail.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-ID: <43ceb427-87aa-44ee-9007-dbaecc499bba@gmail.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: qihao_yewu <qihao_yewu@cmss.chinamobile.com>

--- hw/ufs/ufs.c
+++ hw/ufs/ufs.c
@@ -25,6 +25,7 @@
 #include "qapi/error.h"
 #include "migration/vmstate.h"
 #include "scsi/constants.h"
+#include "hw/irq.h"
 #include "trace.h"
 #include "ufs.h"
 
@@ -1285,6 +1286,8 @@ static void ufs_realize(PCIDevice *pci_dev, Error **errp)
 static void ufs_exit(PCIDevice *pci_dev)
 {
     UfsHc *u = UFS(pci_dev);
+
+    qemu_free_irq(u->irq);
 
     qemu_bh_delete(u->doorbell_bh);
     qemu_bh_delete(u->complete_bh);
