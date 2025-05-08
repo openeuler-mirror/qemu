e4c3799a232a02fc100ee1a2255a66cdd3be123a
Xiaoyao Li <xiaoyao.li@intel.com>
2025-05-08 10:59:39 -0400

    i386/cpu: Introduce enable_cpuid_0x1f to force exposing CPUID 0x1f
    commit ab8bd85adf75900edc2764d0ebe8b53867cc54aa upstream.
    
    Currently, QEMU exposes CPUID 0x1f to guest only when necessary, i.e.,
    when topology level that cannot be enumerated by leaf 0xB, e.g., die or
    module level, are configured for the guest, e.g., -smp xx,dies=2.
    
    However, TDX architecture forces to require CPUID 0x1f to configure CPU
    topology.
    
    Introduce a bool flag, enable_cpuid_0x1f, in CPU for the case that
    requires CPUID leaf 0x1f to be exposed to guest.
    
    Introduce a new function x86_has_cpuid_0x1f(), which is the wrapper of
    cpu->enable_cpuid_0x1f and x86_has_extended_topo() to check if it needs
    to enable cpuid leaf 0x1f for the guest.
    
    Intel-SIG: commit ab8bd85adf75 i386/cpu: Introduce enable_cpuid_0x1f to force exposing CPUID 0x1f.
    CPU new topology backporting
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Link: https://lore.kernel.org/r/20250508150002.689633-34-xiaoyao.li@intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -1920,7 +1920,7 @@ int kvm_arch_init_vcpu(CPUState *cs)
             break;
         }
         case 0x1f:
-            if (!x86_has_extended_topo(env->avail_cpu_topo)) {
+            if (!x86_has_cpuid_0x1f(env_archcpu(env))) {
                 cpuid_i--;
                 break;
             }
