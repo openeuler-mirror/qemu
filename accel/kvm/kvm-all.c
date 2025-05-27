openeuler-mirror/qemu

commit e549f32b1a88cb9ffdc4fc88fa818854a918498e

hw/arm/virt: support the HDBSS feature
We use QEMU to enable or disable the HDBSS feature during live
migration. We can use the migration-parameter to control the size
of the HDBSS buffer, such as:
	migrate_set_parameter hdbss-buffer-size 3
	info migrate_parameters

Signed-off-by: eillon <yezhenyu2@huawei.com>

migration/migration.h
@@ -48,6 +48,13 @@ struct PostcopyBlocktimeContext;
  */
 #define CLEAR_BITMAP_SHIFT_MAX            31
 
+/*
+ * The default HDBSS size. The value ranges [0, 9].
+ * Set to 0 to disable the HDBSS feature.
+ */
+#define DEFAULT_HDBSS_BUFFER_SIZE          0
+#define MAX_HDBSS_BUFFER_SIZE              9
+
 /* This is an abstraction of a "temp huge page" for postcopy's purpose */
 typedef struct {
     /*

migration/ram.c
@@ -39,6 +39,7 @@
 #include "migration-stats.h"
 #include "migration/register.h"
 #include "migration/misc.h"
+#include "migration/options.h"
 #include "qemu-file.h"
 #include "postcopy-ram.h"
 #include "page_cache.h"
@@ -2790,6 +2791,31 @@ static void xbzrle_cleanup(void)
     XBZRLE_cache_unlock();
 }
 
+static void kvm_update_hdbss_cap(bool enable)
+{
+    KVMState *s = kvm_state;
+    int size, ret;
+
+    if (s == NULL || !kvm_check_extension(s, KVM_CAP_ARM_HW_DIRTY_STATE_TRACK)) {
+        return;
+    }
+
+    size = migrate_hdbss_buffer_size();
+    if (size < 0 || size > MAX_HDBSS_BUFFER_SIZE) {
+        fprintf(stderr, "Invalid hdbss buffer size: %d\n", size);
+        return;
+    }
+
+    ret = kvm_vm_enable_cap(s, KVM_CAP_ARM_HW_DIRTY_STATE_TRACK, 0,
+                            enable ? size : 0);
+    if (ret) {
+        fprintf(stderr, "Could not %s KVM_CAP_ARM_HW_DIRTY_STATE_TRACK: %d\n",
+                        enable ? "enable" : "disable", ret);
+    }
+
+    return;
+}
+
 static void ram_save_cleanup(void *opaque)
 {
     RAMState **rsp = opaque;
@@ -2806,6 +2832,7 @@ static void ram_save_cleanup(void *opaque)
              * memory_global_dirty_log_stop will assert that
              * memory_global_dirty_log_start/stop used in pairs
              */
+            kvm_update_hdbss_cap(false);
             memory_global_dirty_log_stop(GLOBAL_DIRTY_MIGRATION);
         }
     }
@@ -3209,6 +3236,7 @@ static void ram_init_bitmaps(RAMState *rs)
         ram_list_init_bitmaps();
         /* We don't use dirty log with background snapshots */
         if (!migrate_background_snapshot()) {
+            kvm_update_hdbss_cap(true);
             memory_global_dirty_log_start(GLOBAL_DIRTY_MIGRATION);
             migration_bitmap_sync_precopy(rs, false);
         }

linux-headers/linux/kvm.h
@@ -1212,6 +1212,8 @@ struct kvm_ppc_resize_hpt {
 /* support request to inject secret to CSV3 guest */
 #define KVM_CAP_HYGON_COCO_EXT_CSV3_INJ_SECRET    (1 << 2)
 
+#define KVM_CAP_ARM_HW_DIRTY_STATE_TRACK 502
+
 #define KVM_CAP_ARM_VIRT_MSI_BYPASS 799
 
 #define KVM_EXIT_HYPERCALL_VALID_MASK (1 << KVM_HC_MAP_GPA_RANGE)

migration/migration-hmp-cmds.c
@@ -409,6 +409,11 @@ void hmp_info_migrate_parameters(Monitor *mon, const QDict *qdict)
         monitor_printf(mon, "%s: %s\n",
             MigrationParameter_str(MIGRATION_PARAMETER_SEV_AMD_CERT),
             params->sev_amd_cert);
+
+        assert(params->has_hdbss_buffer_size);
+        monitor_printf(mon, "%s: %u\n",
+            MigrationParameter_str(MIGRATION_PARAMETER_HDBSS_BUFFER_SIZE),
+            params->hdbss_buffer_size);
     }
 
     qapi_free_MigrationParameters(params);
@@ -724,6 +729,10 @@ void hmp_migrate_set_parameter(Monitor *mon, const QDict *qdict)
         p->sev_amd_cert = g_new0(StrOrNull, 1);
         p->sev_amd_cert->type = QTYPE_QSTRING;
         visit_type_str(v, param, &p->sev_amd_cert->u.s, &err);
+        break;
+    case MIGRATION_PARAMETER_HDBSS_BUFFER_SIZE:
+        p->has_hdbss_buffer_size = true;
+        visit_type_uint8(v, param, &p->hdbss_buffer_size, &err);
         break;
     default:
         assert(0);

migration/options.c
@@ -186,6 +186,9 @@ Property migration_properties[] = {
     DEFINE_PROP_STRING("sev-pdh", MigrationState, parameters.sev_pdh),
     DEFINE_PROP_STRING("sev-plat-cert", MigrationState, parameters.sev_plat_cert),
     DEFINE_PROP_STRING("sev-amd-cert", MigrationState, parameters.sev_amd_cert),
+    DEFINE_PROP_UINT8("hdbss-buffer-size", MigrationState,
+                      parameters.hdbss_buffer_size,
+                      DEFAULT_HDBSS_BUFFER_SIZE),
 
     /* Migration capabilities */
     DEFINE_PROP_MIG_CAP("x-xbzrle", MIGRATION_CAPABILITY_XBZRLE),
@@ -853,6 +856,13 @@ MigMode migrate_mode(void)
     return s->parameters.mode;
 }
 
+int migrate_hdbss_buffer_size(void)
+{
+    MigrationState *s = migrate_get_current();
+
+    return s->parameters.hdbss_buffer_size;
+}
+
 int migrate_multifd_channels(void)
 {
     MigrationState *s = migrate_get_current();
@@ -1032,6 +1042,8 @@ MigrationParameters *qmp_query_migrate_parameters(Error **errp)
     params->vcpu_dirty_limit = s->parameters.vcpu_dirty_limit;
     params->has_mode = true;
     params->mode = s->parameters.mode;
+    params->has_hdbss_buffer_size = true;
+    params->hdbss_buffer_size = s->parameters.hdbss_buffer_size;
 
     return params;
 }
@@ -1069,6 +1081,7 @@ void migrate_params_init(MigrationParameters *params)
     params->has_x_vcpu_dirty_limit_period = true;
     params->has_vcpu_dirty_limit = true;
     params->has_mode = true;
+    params->has_hdbss_buffer_size = true;
 
     params->sev_pdh = g_strdup("");
     params->sev_plat_cert = g_strdup("");
@@ -1415,6 +1428,10 @@ static void migrate_params_test_apply(MigrateSetParameters *params,
         assert(params->sev_amd_cert->type == QTYPE_QSTRING);
         dest->sev_amd_cert = params->sev_amd_cert->u.s;
     }
+
+    if (params->has_hdbss_buffer_size) {
+        dest->hdbss_buffer_size = params->hdbss_buffer_size;
+    }
 }
 
 static void migrate_params_apply(MigrateSetParameters *params, Error **errp)
@@ -1578,6 +1595,10 @@ static void migrate_params_apply(MigrateSetParameters *params, Error **errp)
         g_free(s->parameters.sev_amd_cert);
         assert(params->sev_amd_cert->type == QTYPE_QSTRING);
         s->parameters.sev_amd_cert = g_strdup(params->sev_amd_cert->u.s);
+    }
+
+    if (params->has_hdbss_buffer_size) {
+        s->parameters.hdbss_buffer_size = params->hdbss_buffer_size;
     }
 }
 

migration/options.h
@@ -85,6 +85,7 @@ uint64_t migrate_max_bandwidth(void);
 uint64_t migrate_avail_switchover_bandwidth(void);
 uint64_t migrate_max_postcopy_bandwidth(void);
 MigMode migrate_mode(void);
+int migrate_hdbss_buffer_size(void);
 int migrate_multifd_channels(void);
 MultiFDCompression migrate_multifd_compression(void);
 int migrate_multifd_zlib_level(void);

commit ff64aed3c87427dfa65fa85aef93b44372aefe7d

hw/arm/virt: HDBSS: fix arm-softmmu build on x86 platform
Move kvm_update_hdbss_cap() to accel/kvm/kvm-stub.c,
check kvm_enabled() and add stub function

Fixes: e549f32b1a88 ("hw/arm/virt: support the HDBSS feature")
Signed-off-by: Jason Zeng <jason.zeng@intel.com>

accel/kvm/kvm-all.c
@@ -3251,6 +3251,31 @@ bool kvm_arm_supports_user_irq(void)
     return kvm_check_extension(kvm_state, KVM_CAP_ARM_USER_IRQ);
 }
 
+void kvm_update_hdbss_cap(bool enable, int hdbss_buffer_size)
+{
+    KVMState *s = kvm_state;
+    int size, ret;
+
+    if (s == NULL || !kvm_check_extension(s, KVM_CAP_ARM_HW_DIRTY_STATE_TRACK)) {
+        return;
+    }
+
+    size = hdbss_buffer_size;
+    if (size < 0 || size > MAX_HDBSS_BUFFER_SIZE) {
+        fprintf(stderr, "Invalid hdbss buffer size: %d\n", size);
+        return;
+    }
+
+    ret = kvm_vm_enable_cap(s, KVM_CAP_ARM_HW_DIRTY_STATE_TRACK, 0,
+                            enable ? size : 0);
+    if (ret) {
+        fprintf(stderr, "Could not %s KVM_CAP_ARM_HW_DIRTY_STATE_TRACK: %d\n",
+                        enable ? "enable" : "disable", ret);
+    }
+
+    return;
+}
+
 #ifdef KVM_CAP_SET_GUEST_DEBUG
 struct kvm_sw_breakpoint *kvm_find_sw_breakpoint(CPUState *cpu, vaddr pc)
 {

accel/stubs/kvm-stub.c
@@ -119,6 +119,11 @@ bool kvm_arm_supports_user_irq(void)
     return false;
 }
 
+void kvm_update_hdbss_cap(bool enable, int hdbss_buffer_size)
+{
+    g_assert_not_reached();
+}
+
 bool kvm_dirty_ring_enabled(void)
 {
     return false;

include/sysemu/kvm.h
@@ -229,6 +229,14 @@ int kvm_has_gsi_routing(void);
  */
 bool kvm_arm_supports_user_irq(void);
 
+/*
+ * The default HDBSS size. The value ranges [0, 9].
+ * Set to 0 to disable the HDBSS feature.
+ */
+#define DEFAULT_HDBSS_BUFFER_SIZE          0
+#define MAX_HDBSS_BUFFER_SIZE              9
+
+void kvm_update_hdbss_cap(bool enable, int hdbss_buffer_size);
 
 int kvm_on_sigbus_vcpu(CPUState *cpu, int code, void *addr);
 int kvm_on_sigbus(int code, void *addr);

migration/migration.h
@@ -48,13 +48,6 @@ struct PostcopyBlocktimeContext;
  */
 #define CLEAR_BITMAP_SHIFT_MAX            31
 
-/*
- * The default HDBSS size. The value ranges [0, 9].
- * Set to 0 to disable the HDBSS feature.
- */
-#define DEFAULT_HDBSS_BUFFER_SIZE          0
-#define MAX_HDBSS_BUFFER_SIZE              9
-
 /* This is an abstraction of a "temp huge page" for postcopy's purpose */
 typedef struct {
     /*

migration/ram.c
@@ -2812,33 +2812,6 @@ static void xbzrle_cleanup(void)
     XBZRLE_cache_unlock();
 }
 
-#ifdef TARGET_AARCH64
-static void kvm_update_hdbss_cap(bool enable, int hdbss_buffer_size)
-{
-    KVMState *s = kvm_state;
-    int size, ret;
-
-    if (s == NULL || !kvm_check_extension(s, KVM_CAP_ARM_HW_DIRTY_STATE_TRACK)) {
-        return;
-    }
-
-    size = hdbss_buffer_size;
-    if (size < 0 || size > MAX_HDBSS_BUFFER_SIZE) {
-        fprintf(stderr, "Invalid hdbss buffer size: %d\n", size);
-        return;
-    }
-
-    ret = kvm_vm_enable_cap(s, KVM_CAP_ARM_HW_DIRTY_STATE_TRACK, 0,
-                            enable ? size : 0);
-    if (ret) {
-        fprintf(stderr, "Could not %s KVM_CAP_ARM_HW_DIRTY_STATE_TRACK: %d\n",
-                        enable ? "enable" : "disable", ret);
-    }
-
-    return;
-}
-#endif
-
 static void ram_save_cleanup(void *opaque)
 {
     RAMState **rsp = opaque;
@@ -2856,7 +2829,9 @@ static void ram_save_cleanup(void *opaque)
              * memory_global_dirty_log_start/stop used in pairs
              */
 #ifdef TARGET_AARCH64
-            kvm_update_hdbss_cap(false, 0);
+            if (kvm_enabled()) {
+                kvm_update_hdbss_cap(false, 0);
+            }
 #endif
             memory_global_dirty_log_stop(GLOBAL_DIRTY_MIGRATION);
         }
@@ -3262,7 +3237,9 @@ static void ram_init_bitmaps(RAMState *rs)
         /* We don't use dirty log with background snapshots */
         if (!migrate_background_snapshot()) {
 #ifdef TARGET_AARCH64
-            kvm_update_hdbss_cap(true, migrate_hdbss_buffer_size());
+            if (kvm_enabled()) {
+                kvm_update_hdbss_cap(true, migrate_hdbss_buffer_size());
+            }
 #endif
             memory_global_dirty_log_start(GLOBAL_DIRTY_MIGRATION);
             migration_bitmap_sync_precopy(rs, false);