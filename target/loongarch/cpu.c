ad4d83146b6dc18f85512826e56a6be19f897196
Bibo Mao <maobibo@loongson.cn>
2025-10-30 16:17:23 +0800

    target/loongarch: Call function loongarch_la464_init_csr() after realized
    When CPU is realized, it will check capability of host and set guest
    features, such as PMU CSR register number used by VM etc. Here move
    function call with loongarch_la464_init_csr() after CPU is realized.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Reviewed-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>

--- target/loongarch/cpu.c
+++ target/loongarch/cpu.c
@@ -376,11 +376,11 @@ static bool loongarch_cpu_has_work(CPUState *cs)
 #endif
 }
 
-static void loongarch_la464_init_csr(Object *obj)
+static void loongarch_la464_init_csr(DeviceState *dev)
 {
 #ifndef CONFIG_USER_ONLY
     static bool initialized;
-    LoongArchCPU *cpu = LOONGARCH_CPU(obj);
+    LoongArchCPU *cpu = LOONGARCH_CPU(dev);
     CPULoongArchState *env = &cpu->env;
     int i, num;
 
@@ -647,7 +647,6 @@ static void loongarch_la464_initfn(Object *obj)
 
     cpu->msgint = ON_OFF_AUTO_OFF;
     cpu->ptw = ON_OFF_AUTO_OFF;
-    loongarch_la464_init_csr(obj);
     loongarch_cpu_post_init(obj);
 }
 
@@ -819,6 +818,7 @@ static void loongarch_cpu_realizefn(DeviceState *dev, Error **errp)
 
     qemu_init_vcpu(cs);
     cpu_reset(cs);
+    loongarch_la464_init_csr(dev);
 
     lacc->parent_realize(dev, errp);
 }
