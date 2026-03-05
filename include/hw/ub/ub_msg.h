37c4e7d04d9331481c067af895d6710f74731209
xiangzixuan123 <xiangzixuan@huawei.com>
2026-03-05 16:27:47 +0800

    ub: support process msg code exch
    support process msg code exch
    
    Signed-off-by: xiangzixuan123 <xiangzixuan@huawei.com>

--- include/hw/ub/ub_msg.h
+++ include/hw/ub/ub_msg.h
@@ -15,8 +15,8 @@
  * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef UB_POOL_H
-#define UB_POOL_H
+#ifndef UB_MSG_H
+#define UB_MSG_H
 
 enum ub_pool_sub_msg_code {
     UB_DEV_REG,
@@ -25,5 +25,34 @@ enum ub_pool_sub_msg_code {
     UB_BI_DESTROY,
     UB_CFG_CPL_NOTIFY,
 };
+
+enum UbExchSubMsgCode {
+    UB_OBTAIN_ENTITY_INFO = 2,
+    UB_LINK_NEIGHBOR_QUERY = 7,
+    UB_EXCH_MAX_SUB_MSG_CODE
+};
+
+struct UeMap {
+    uint16_t start_entity_idx;
+    uint16_t end_entity_idx;
+};
+
+struct EntityInfoMsgPldRsp {
+    uint32_t reserved;
+    uint16_t entity_nums;
+    uint16_t mue_nums;
+    struct UeMap map[];
+};
+#define ENTITY_INFO_BASE_PLD_SIZE 8
+
+struct EntityInfoMsgPld {
+    /* request payload is NULL */
+    struct EntityInfoMsgPldRsp rsp;
+};
+
+typedef struct EntityInfoMsgPkt {
+    struct MsgPktHeader header;
+    struct EntityInfoMsgPld pld;
+} EntityInfoMsgPkt;
 
 #endif
