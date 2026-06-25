openeuler-mirror/qemu

commit 72c1e9c628fce26582cbef6710c12912eeeabf7f

migration/cgs:fix stream leak on error in virtcca_mig_stream_setup
Free previously allocated streams when virtcca_mig_stream_setup fails

Signed-off-by: lijunwei <lijunwei@kylinos.cn>

migration/cgs-virtcca.c
@@ -690,6 +690,7 @@ static int virtcca_mig_stream_setup(uint32_t nr_channels, uint32_t nr_pages)
         if (!ret) {
             virtCCA_mig.nr_streams++;
         } else {
+	    	virtcca_mig_cleanup();
             return ret;
         }
     }

commit a84f57279061d6dadf67d3ef5c83fe3ba997f435

migration/virtcca: add forward declaration for virtcca_mig_cleanup
virtcca_mig_cleanup() is called from virtcca_mig_stream_setup() before
its definition appears later in cgs-virtcca.c. Because the helper is
declared static, the compiler has no visible prototype at the call site,
which triggers an implicit-declaration error and breaks the build under
-Werror=implicit-function-declaration, --enable-virtcca-migration.

Add a forward declaration near the top of the file so the static helper
is visible to all callers regardless of definition order.

Fixes: 72c1e9c628fce26582cbef6710c12912eeeabf7f (migration/cgs:fix stream leak on error in virtcca_mig_stream_setup)
Signed-off-by: Panhengchang panhengchang@huawei.com
Reported-by: Panhengchang panhengchang@huawei.com

migration/cgs-virtcca.c
@@ -81,6 +81,7 @@ typedef union GpaListEntry {
     };
 } GpaListEntry;
 
+static void virtcca_mig_cleanup(void);
 
 virtCCAMigState virtCCA_mig;
 