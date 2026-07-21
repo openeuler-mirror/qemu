openeuler-mirror/qemu

commit f797e2713a94b48de59324d00c851d89f4438fc0

tcg/mttcg: enable threads to unregister in tcg_ctxs[]
[This patch is just for reference. It has problems as it does not takes care of
the TranslationBlocks and their assigned regions during CPU unrealize]

When using TCG acceleration in a multi-threaded context each vCPU has its own
thread registered in tcg_ctxs[] upon creation and tcg_cur_ctxs stores the current
number of threads that got created. Although, the lack of a mechanism to
unregister these threads is a problem when exercising vCPU hotplug/unplug
due to the fact that tcg_cur_ctxs gets incremented everytime a vCPU gets
hotplugged but never gets decremented everytime a vCPU gets unplugged, therefore
breaking the assert stating tcg_cur_ctxs < tcg_max_ctxs after a certain amount
of vCPU hotplugs.

Suggested-by: Salil Mehta <salil.mehta@huawei.com>
[SM: Check Things To Do Section, https://lore.kernel.org/all/20200613213629.21984-1-salil.mehta@huawei.com/]
Signed-off-by: Miguel Luis <miguel.luis@oracle.com>

tcg/tcg.c
@@ -764,6 +764,14 @@ static void alloc_tcg_plugin_context(TCGContext *s)
 #endif
 }
 
+static void free_tcg_plugin_context(TCGContext *s)
+{
+#ifdef CONFIG_PLUGIN
+    g_ptr_array_unref(s->plugin_tb->insns);
+    g_free(s->plugin_tb);
+#endif
+}
+
 /*
  * All TCG threads except the parent (i.e. the one that called tcg_context_init
  * and registered the target's TCG globals) must register with this function
@@ -813,6 +821,21 @@ void tcg_register_thread(void)
     }
 
     tcg_ctx = s;
+}
+
+void tcg_unregister_thread(void)
+{
+    TCGContext *s = tcg_ctx;
+    unsigned int n;
+
+    /* Unclaim an entry in tcg_ctxs */
+    n = qatomic_fetch_dec(&tcg_cur_ctxs);
+    g_assert(n > 1);
+    qatomic_store_release(&tcg_ctxs[n - 1], 0);
+
+    free_tcg_plugin_context(s);
+
+    g_free(s);
 }
 #endif /* !CONFIG_USER_ONLY */
 

accel/tcg/tcg-accel-ops-mttcg.c
@@ -122,6 +122,7 @@ static void *mttcg_cpu_thread_fn(void *arg)
     qemu_mutex_unlock_iothread();
     rcu_remove_force_rcu_notifier(&force_rcu.notifier);
     rcu_unregister_thread();
+    tcg_unregister_thread();
     return NULL;
 }
 

include/tcg/startup.h
@@ -46,6 +46,11 @@ void tcg_init(size_t tb_size, int splitwx, unsigned max_cpus);
 void tcg_register_thread(void);
 
 /**
+ * tcg_register_thread: Unregister this thread with the TCG runtime
+ */
+void tcg_unregister_thread(void);
+
+/**
  * tcg_prologue_init(): Generate the code for the TCG prologue
  *
  * In softmmu this is done automatically as part of the TCG

commit 62ba8a79b90707c7e0c76d8e4f3fcea2667b51db

tcg: move free_tcg_plugin_context to the correct macro range
`free_tcg_plugin_context` is used in `!CONFIG_USER_ONLY`.
Move it to the macro range.

Build werror:

../tcg/tcg.c:767:13: error: ‘free_tcg_plugin_context’ defined but not used [-Werror=unused-function]
  767 | static void free_tcg_plugin_context(TCGContext *s)
      |             ^~~~~~~~~~~~~~~~~~~~~~~
cc1: all warnings being treated as errors

Fixes: f797e2713(tcg/mttcg: enable threads to unregister in tcg_ctxs[])
Signed-off-by: liuxiangdong <liuxiangdong5@huawei.com>

tcg/tcg.c
@@ -764,14 +764,6 @@ static void alloc_tcg_plugin_context(TCGContext *s)
 #endif
 }
 
-static void free_tcg_plugin_context(TCGContext *s)
-{
-#ifdef CONFIG_PLUGIN
-    g_ptr_array_unref(s->plugin_tb->insns);
-    g_free(s->plugin_tb);
-#endif
-}
-
 /*
  * All TCG threads except the parent (i.e. the one that called tcg_context_init
  * and registered the target's TCG globals) must register with this function
@@ -821,6 +813,14 @@ void tcg_register_thread(void)
     }
 
     tcg_ctx = s;
+}
+
+static void free_tcg_plugin_context(TCGContext *s)
+{
+#ifdef CONFIG_PLUGIN
+    g_ptr_array_unref(s->plugin_tb->insns);
+    g_free(s->plugin_tb);
+#endif
 }
 
 void tcg_unregister_thread(void)